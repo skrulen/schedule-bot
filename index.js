a39967bd4093c27e9fb228bee0d549c66a6a25cc
skrulen <skrulen@mail.ru>
2023-01-19 21:20:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const TelegramApi = require('node-telegram-bot-api')
+
+const token = '5976146333:AAFs4OZ_dLAEuYiyI3hgQyRs_5CYAL7vq8M'
+
+const bot = new TelegramApi(token, {polling: true})
+
+
+let info;
+bot.on('message', msg => {
+  const text = msg.text;
+  const uid = msg.chat.id;
+  console.log(`${msg.from.username}: ${text}`);
+  console.log(msg);
+})
